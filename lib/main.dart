fae4b53bd3490083930c672c02e08d72cc81e58a
HeroRickyGAMES <ricojn9@gmail.com>
2023-01-26 11:30:39 -0300

    Usando Map nas perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,19 @@ class _PerguntaAppState extends State<PerguntaApp>{
   @override
   Widget build(BuildContext context) {
 
-    final perguntas = [
-      "Qual é a sua cor favorita?",
-      "Qual é o seu animal favorito?",
+    final List<Map <String, Object>> perguntas = [
+     {
+       'texto': 'Qual é a sua cor favorita?',
+       'respostas': ['Preto', 'Vermelho', 'Verde','Branco'],
+     },
+      {
+        'texto': "Qual é o seu animal favorito?",
+        'respostas': ['Coelho', 'Cobra', 'Elefante','Leão'],
+      },
+      {
+        'texto': "Porque o Jailson Mendes demorou para entregar a peça para o Kauan para relaxar?",
+        'respostas': ['Foi dar a bunda', 'Foi cagar no mato antes de dar', 'Foi buscar na China','Foi buscar na India'],
+      },
     ];
 
     return MaterialApp(
@@ -36,7 +46,7 @@ class _PerguntaAppState extends State<PerguntaApp>{
         ),
         body: Column(
           children: [
-            Questao(perguntas[PerguntaSelecionada]),
+            Questao(perguntas[PerguntaSelecionada]['texto'] as String),
             Resposta("Resposta 1", responder),
             Resposta("Resposta 2", responder),
             Resposta("Resposta 3", responder)
